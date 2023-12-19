1cc195d21271d78785a7191da9d86c103a4b4969
Hoang Nguyen <Hoang.Viet.Nguyen@outlook.com>
2023-12-18 22:40:02 -0500

    added postgres database using docker in place of in-memory solution.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using starter_api.Models;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PSQL")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
